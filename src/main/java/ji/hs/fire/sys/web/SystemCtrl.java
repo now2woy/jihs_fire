b0fa024a5ecf76e00ccc798c01ca991aef5eb123
now2w <now2w@DESKTOP-O2VVPQP>
2022-11-13 12:39:32 +0900

    ..

---
+++ src/main/java/ji/hs/fire/sys/web/SystemCtrl.java
@@ -0,0 +1,30 @@
+package ji.hs.fire.sys.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 시스템 CTRL
+ * @author now2w
+ *
+ */
+@Slf4j
+@RestController
+public class SystemCtrl {
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	/**
+	 * 시스템을 종료한다.
+	 */
+	@DeleteMapping("/sys/shutdown")
+	public void shutdown() {
+		log.info("시스템을 종료 합니다.");
+		SpringApplication.exit(applicationContext, () -> 0);
+	}
+}
