bdf9af312172c3b21744d11a52ac75b0ddb0da1d
now2w <now2w@DESKTOP-O2VVPQP>
2022-11-21 22:41:41 +0900

    .

--- src/main/java/ji/hs/fire/config/MyBatisConfig.java
+++ src/main/java/ji/hs/fire/config/MyBatisConfig.java
@@ -9,10 +9,14 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * DB 및 Mybatis 설정
@@ -23,10 +27,30 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 @MapperScan(basePackages = {"ji.hs.fire"})
 public class MyBatisConfig {
 	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
 	private ApplicationContext applicationContext;
+	
+	@Value("${spring.datasource.driverClassName}")
+	private String driverClassName;
+	
+	@Value("${spring.datasource.url}")
+	private String jdbcUrl;
+	
+	@Value("${spring.datasource.username}")
+	private String userName;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+	
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName(driverClassName);
+		hikariConfig.setJdbcUrl(jdbcUrl);
+		hikariConfig.setUsername(userName);
+		hikariConfig.setPassword(password);
+		
+		return new HikariDataSource(hikariConfig);
+	}
 	
 	/**
 	 * 데이터 소스에 트렌젝션 적용
@@ -34,7 +58,7 @@ public class MyBatisConfig {
 	 */
 	@Bean
 	public DataSourceTransactionManager transactionManager() {
-		return new DataSourceTransactionManager(dataSource);
+		return new DataSourceTransactionManager(dataSource());
 	}
 	
 	/**
@@ -46,7 +70,7 @@ public class MyBatisConfig {
 	public SqlSessionFactoryBean sqlSessionFactory() throws IOException {
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 
-		factoryBean.setDataSource(dataSource);
+		factoryBean.setDataSource(dataSource());
 		factoryBean.setConfigLocation(applicationContext.getResource("classpath:/mybatis/mybatis-config.xml"));
 		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis/mapper/**/*Mapper.xml"));
 		
